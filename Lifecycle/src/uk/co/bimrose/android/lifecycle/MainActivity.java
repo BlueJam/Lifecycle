14c0512a5732b9b9ac8e49f1b92e278f7ea2dbdf
BlueJam <bimrose@gmail.com>
2013-11-01 14:27:40 +0000

    Update to make code more efficient.

--- Lifecycle/src/uk/co/bimrose/android/lifecycle/MainActivity.java
+++ Lifecycle/src/uk/co/bimrose/android/lifecycle/MainActivity.java
@@ -1,61 +1,22 @@
 package uk.co.bimrose.android.lifecycle;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends LifecycleLoggingActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Log.d(getClass().getSimpleName(), "onCreate()");
 	}
 
 	public void start(View v) {
 		Intent i = new Intent(this, Other.class);
 		i.putExtra(Other.MAIN_DATA, getString(R.string.other));
 		startActivity(i);
-	}
-
-	@Override
-	public void onRestart() {
-		super.onRestart();
-		Log.d(getClass().getSimpleName(), "onRestart()");
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-		Log.d(getClass().getSimpleName(), "onStart()");
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		Log.d(getClass().getSimpleName(), "onResume()");
-	}
-
-	@Override
-	public void onPause() {
-		Log.d(getClass().getSimpleName(), "onPause()");
-		super.onPause();
-	}
-
-	@Override
-	public void onStop() {
-		Log.d(getClass().getSimpleName(), "onStop()");
-		super.onStop();
-	}
-
-	@Override
-	public void onDestroy() {
-		Log.d(getClass().getSimpleName(), "onDestroy()");
-		super.onDestroy();
 	}
 
 }
--- Lifecycle/src/uk/co/bimrose/android/lifecycle/Other.java
+++ Lifecycle/src/uk/co/bimrose/android/lifecycle/Other.java
@@ -14,43 +14,6 @@ public class Other extends Activity {
 		setContentView(R.layout.other);
 		TextView tv = (TextView) findViewById(R.id.textView);
 		tv.setText(getIntent().getStringExtra(MAIN_DATA));
-
-		Log.d(getClass().getSimpleName(), "onCreate()");
-	}
-
-	public void onRestart() {
-		super.onRestart();
-		Log.d(getClass().getSimpleName(), "onRestart()");
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-		Log.d(getClass().getSimpleName(), "onStart()");
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		Log.d(getClass().getSimpleName(), "onResume()");
-	}
-
-	@Override
-	public void onPause() {
-		Log.d(getClass().getSimpleName(), "onPause()");
-		super.onPause();
-	}
-
-	@Override
-	public void onStop() {
-		Log.d(getClass().getSimpleName(), "onStop()");
-		super.onStop();
-	}
-
-	@Override
-	public void onDestroy() {
-		Log.d(getClass().getSimpleName(), "onDestroy()");
-		super.onDestroy();
 	}
 
 }
